b327fc01b41db992104a831b23b1499b81a3ce09
Dawna Backmeier <karanacesin@yahoo.com>
2020-07-30 11:56:05 -0400

    changes to knex

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
-// Update with your config settings.
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/recipeuser";
+
 
 module.exports = {
 
@@ -38,19 +39,18 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "pg",
+    connection: pgConnection,
     pool: {
-      min: 2,
-      max: 10
+        min: 2,
+        max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+        directory: "./data/migrations",
+    },
+    seeds: {
+        directory: "./data/seeds",
+    },
+  },
 
 };
